886497ddccf94f464598fdfe35e0cef22f509470
haedaraifan <haedaraifan@gmail.com>
2024-04-24 08:11:31 +0700

    fix: [MA] user api route

--- routes/api.php
+++ routes/api.php
@@ -7,13 +7,13 @@ use App\Http\Middleware\ApiAuthMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post("/users/register", [UserController::class, "register"]);
-Route::post("/users/login", [UserController::class, "login"]);
+Route::post("/register", [UserController::class, "register"]);
+Route::post("/login", [UserController::class, "login"]);
 
 Route::middleware(ApiAuthMiddleware::class)->group(function() {
-    Route::get("/users/me", [UserController::class, "get"]);
-    Route::post("/users/me", [UserController::class, "update"]);
-    Route::delete("/users/logout", [UserController::class, "logout"]);
+    Route::get("/me", [UserController::class, "get"]);
+    Route::post("/me", [UserController::class, "update"]);
+    Route::delete("/logout", [UserController::class, "logout"]);
 
     Route::post("/products", [ProductController::class,"create"]);
     Route::get("/products", [ProductController::class,"list"]);
