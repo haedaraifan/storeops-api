68feb29a0e3567ef92568e8defd17459bcfdeae2
haedaraifan <haedaraifan@gmail.com>
2024-05-17 23:19:39 +0700

    fix: [MA] list note without foreign to user table

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -6,7 +6,6 @@ use App\Http\Requests\NoteCreateRequest;
 use App\Http\Resources\NoteResource;
 use App\Models\Note;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class NoteController extends Controller
@@ -23,8 +22,7 @@ class NoteController extends Controller
 
     public function list(Request $request): JsonResponse
     {
-        $user = Auth::user();
-        $notes = $user->notes;
+        $notes = Note::get();
 
         return (NoteResource::collection($notes))->response()->setStatusCode(200);
     }
