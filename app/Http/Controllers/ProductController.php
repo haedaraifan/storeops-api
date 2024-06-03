4f99b29c73f62e156595439d346305892d0203db
haedaraifan <haedaraifan@gmail.com>
2024-06-03 10:39:40 +0700

    fix: [MA] produck restock controller missing code

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -119,6 +119,7 @@ class ProductController extends Controller
         $restockProductHistory = new RestockProductHistory($data);
         $restockProductHistory->name = $product->name;
         $restockProductHistory->unit = $product->unit->name;
+        $restockProductHistory->category = $product->category;
         $restockProductHistory->purchase_price = $product->purchase_price;
         $restockProductHistory->selling_price = $product->selling_price;
         $restockProductHistory->save();
